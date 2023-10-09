0329502162b1cc55b1da5203b2926d67c3d30f06
AuroraLantean <raymondlieu.crypto@runbox.com>
2023-10-09 11:36:00 +0800

    fix ItemT -> BoxT in api/item/[ctrt_addr]/route.ts

--- app/api/item/route.ts
+++ app/api/item/route.ts
@@ -1,10 +1,6 @@
-import { ItemT } from '@/types'
+import { BoxT } from '@/types'
 import { NextResponse } from 'next/server'
-import item_data from '@/mockdata/item_data.json'
-
-const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/items"
-
-const API_KEY: string = process.env.DATA_API_KEY as string
+import box_data from '@/mockdata/box_data.json'
 
 const delayFunc = (delay: number): Promise<boolean> => new Promise((resolve, reject) => setTimeout(() => {
   console.log("delay:", delay);
@@ -12,9 +8,9 @@ const delayFunc = (delay: number): Promise<boolean> => new Promise((resolve, rej
 }, delay))
 
 const mockResponse = () => {
-  const itemOut = { "item_id": "161SxAvUwpMNVoCUUqbn8vGRQEnSGjN6Z6", "seller_id": "18xBe5NdALYzWoHN9QVWLjK8DUxzKQcc3r", "title": "felis ut", "img_url": "https://techcrunch.com/integer/", "fixed_price": 20, "min_price": 19, "bid_price": 43, "votes": 89, "status": "success" };
-  //console.log(item_data[0])
-  const myBlob = new Blob([JSON.stringify(itemOut, null, 2)], { type: 'application/json' });
+  const box = { "id": 7, "title": "integer", "seller_id": "9nXgU0GWjowSDQfR", "available": 74, "total": 292, "status": "open", "detail_link": "http://angelfire.com/tristique/tortor.js", "img_link": "http://dummyimage.com/209x119.png/5fa2dd/ffffff", "compo_addr": "1MMjjVKgYv5FovSXGz1VMkmnyM4QsHvzKs", "interest": 13.94, "fixed_price": 428.14, "min_price": 382.18, "bid_price": 179.7, "votes": 47 };
+  //console.log(box_data[0])
+  const myBlob = new Blob([JSON.stringify(box, null, 2)], { type: 'application/json' });
 
   const myOptions = { "status": 200, "statusText": "SuperSmashingGreat!" };
   const res = new Response(myBlob, myOptions);
@@ -23,15 +19,15 @@ const mockResponse = () => {
 //http://localhost:3000/api/item/
 export const GET = async () => {
   //const res = await fetch(DATA_SOURCE_URL)
-  //const items: ItemT[] = await res.json()
-  let items: ItemT[];
+  //const boxes: BoxT[] = await res.json()
+  let boxes: BoxT[];
   await delayFunc(1000)
-  items = item_data;
-  return NextResponse.json(items)
+  boxes = box_data;
+  return NextResponse.json(boxes)
 }
 
 export async function POST(request: Request) {
-  const { seller_id, title }: Partial<ItemT> = await request.json()
+  const { seller_id, title }: Partial<BoxT> = await request.json()
   console.log("🚀 POST ~ seller_id, title:", seller_id, title)
 
   if (!seller_id || !title) return NextResponse.json({ "message": "Missing required data" })
@@ -49,21 +45,21 @@ export async function POST(request: Request) {
     })
   })
 */
-  const newTodo: ItemT = await res.json()
+  const newTodo: BoxT = await res.json()
 
   return NextResponse.json(newTodo)
 }
 
 export async function PUT(request: Request) {
-  const { seller_id, item_id, title, status }: ItemT = await request.json()
-  console.log("🚀 PUT ~ seller_id, item_id, title, status:", seller_id, item_id, title, status)
+  const { seller_id, id, title, status }: BoxT = await request.json()
+  console.log("🚀 PUT ~ seller_id, id, title, status:", seller_id, id, title, status)
 
   //|| typeof (status) !== 'boolean'
-  if (!seller_id || !item_id || !title) return NextResponse.json({ "message": "Missing required data" })
+  if (!seller_id || !id || !title) return NextResponse.json({ "message": "Missing required data" })
 
   await delayFunc(1000);
   const res = mockResponse()
-  /*const res = await fetch(`${DATA_SOURCE_URL}/${item_id}`, {
+  /*const res = await fetch(`${DATA_SOURCE_URL}/${id}`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
@@ -74,20 +70,20 @@ export async function PUT(request: Request) {
     })
   })*/
 
-  const updatedTodo: ItemT = await res.json()
+  const updatedTodo: BoxT = await res.json()
 
   return NextResponse.json(updatedTodo)
 }
 
 export async function DELETE(request: Request) {
-  const { item_id }: Partial<ItemT> = await request.json()
-  console.log("🚀 DELETE ~ item_id:", item_id)
+  const { id }: Partial<BoxT> = await request.json()
+  console.log("🚀 DELETE ~ id:", id)
 
-  if (!item_id) return NextResponse.json({ "message": "ItemT item_id required" })
+  if (!id) return NextResponse.json({ "message": "BoxT id required" })
 
   await delayFunc(1000);
   const res = mockResponse()
-  /*await fetch(`${DATA_SOURCE_URL}/${item_id}`, {
+  /*await fetch(`${DATA_SOURCE_URL}/${id}`, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
@@ -95,5 +91,5 @@ export async function DELETE(request: Request) {
     }
   })*/
 
-  return NextResponse.json({ "message": `ItemT ${item_id} deleted` })
+  return NextResponse.json({ "message": `BoxT ${id} deleted` })
 }
